29d5702d962a0a84405ce13c0be6d530515bd7a9
wulinfeng <1486634154@qq.com>
2021-03-19 17:15:39 +0800

    feat: hitTest方案 LVContainerView 遵循 WKNativelyInteractible 协议

--- WeAppTongCeng/LVTongCeng/LVTongCengWebView.m
+++ WeAppTongCeng/LVTongCeng/LVTongCengWebView.m
@@ -31,13 +31,13 @@
     UIView *wkHitView = [super hitTest:point withEvent:event];
     
     // Handling the WKContentView GestureRecognizers interception event causing the inserted native control to fail to respond
-    // 处理 WKContentView gestureRecognizers 拦截事件,导致插入的原生控件无法响应
     if (!self.didHandleWKContentGestrues) {
         [self _handleWKContentGestrues];
         self.didHandleWKContentGestrues = YES;
     }
     
-    // WKChildScrollView -[hitTest:withEvent:]
+    // Plan A: LVContainerView conforms to protocol WKNativelyInteractible
+    // Plan B: WKChildScrollView -[hitTest:withEvent:]
     if ([wkHitView isKindOfClass:NSClassFromString(@"WKChildScrollView")]) {
         for (UIView *subview in [wkHitView.subviews reverseObjectEnumerator]) {
             CGPoint convertedPoint = [subview convertPoint:point fromView:self];
